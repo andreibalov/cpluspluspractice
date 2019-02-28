1c1a41fdcfb56bd52fa363c1c30bf055ca0ad5cb
AndreiBalov <andreibalov@mail.ru>
2019-02-28 22:28:37 +0300

    linkedlist subdir

---
+++ linkedlist/list.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+class ListNode
+{
+public:
+    ListNode(int item, class ListNode* next = nullptr)
+        : m_item(item), m_next(next)
+    {
+
+    }
+
+    int m_item;
+    ListNode* m_next;
+};
+
+ListNode* reverse(ListNode* head)
+{
+    if (!head)
+    {
+        return head;
+    }
+
+    auto current = head;
+    ListNode* prev = nullptr;
+    while (current)
+    {
+        auto tmpeNext = current->m_next;
+        current->m_next = prev;
+        prev = current;
+        if (!tmpeNext)
+            return current;
+
+        current = tmpeNext;
+    }
+}
