45ab0c01be68f4d83ada22e4012069dee1425721
AndreiBalov <andreibalov@mail.ru>
2019-03-17 23:03:26 +0300

    test option

--- binarysearch/binarysearch.h
+++ binarysearch/binarysearch.h
@@ -5,16 +5,24 @@ int bsearch(const containerT& container, const valueT& value)
 {
     size_t left = 0;
     size_t right = container.size();
-
-    while (left <= right)
+	
+    while (left < right)
     {
-
-
-
+		int mid = left + (right - left) / 2;
+		valueT& data = container[mid];
+		
+		if(data == value)
+			return mid;
+		
+		if(data > value)
+		{
+			right = mid;
+		}
+		else
+		{
+			left = mid + 1;
+		}
     }
-
-
-
 	return -1;
 }
 
